35e5b21ce772effe711a0ac2397db63e5aab991b
samar <benmbareksamar8@gmail.com>
2021-11-02 11:42:08 +0100

    affirmation2

--- app/src/main/java/com/samar/affirmationsapp/model/affirmation.kt
+++ app/src/main/java/com/samar/affirmationsapp/model/affirmation.kt
@@ -1,3 +1,10 @@
 package com.samar.affirmationsapp.model
 
-data class affirmation(val stringResourceId: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class affirmation(
+@StringRes
+val stringResourceId: Int,
+@DrawableRes
+val imageResourceId: Int)
